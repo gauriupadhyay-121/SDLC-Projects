688f56c8562c952987065df32cf2c8a2529f285d
gauriupadhyay-121 <gauriupadhyay@sdlccorp.com>
2026-06-20 09:23:33 +0530

    Second commit

---
+++ RPSgame.js
@@ -0,0 +1,61 @@
+let userscore = 0;
+let compscore = 0;
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userscorepara = document.querySelector("#user-score");
+const compscorepara = document.querySelector("#comp-score");
+
+const gencompchoice = () => {
+    const options = ["rock","paper","scissors"];
+    const randidx =Math.floor(Math.random() * 3);
+    return options[randidx];
+}
+
+const drawgame = () => {
+    msg.innerText = "Game was draw";
+    msg.innerText = "Game was draw. Play again";
+    msg.style.backgroundColor ="#081b31"
+};
+
+const showwinner = (userwin, userchoice, compchoice) => {
+    if(userwin) {
+    userscore++;
+    userscorepara.innerText = userscore;
+    msg.innerText = `You win! Your ${userchoice} beats ${compchoice}`;
+    msg.style.backgroundColor = "green";
+    } else {
+        compscore++;
+        compscorepara.innerText = compscore;
+        msg.innerText = `You lost. ${compchoice} beats your ${userchoice}`;
+        msg.style.backgroundColor = "red";
+    }
+ };
+
+const playgame = (userchoice) => {
+    const compchoice = gencompchoice();
+    
+
+    if(userchoice === compchoice) {
+        drawgame();
+    } else {
+        let userwin = true;
+        if (userchoice === "rock") {
+            userwin = compchoice === "paper" ? false : true;
+        } else if (userchoice === "paper") {
+            userwin = compchoice === "scissors" ? false : true;
+        } else {
+            userwin === "rock" ? false : true;
+        }
+        showwinner(userwin, userchoice, compchoice);
+    }
+};
+
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userchoice = choice.getAttribute("id");
+        playgame(userchoice);
+    });
+});
